65898c7cc9e0a6773fd9a0725567dd398e4f6a8b
Nainish Rai <nainishraisjp@gmail.com>
2023-08-20 15:46:22 +0530

    fixed config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = nodeExternals();
-    }
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
     return config;
   },
 };
